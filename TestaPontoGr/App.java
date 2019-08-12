5ca062576b636f7144d96105bcd662280cb14b5f
Victor Barbosa Bulhões <44437190+VictorBBulhoes@users.noreply.github.com>
2019-08-12 09:45:34 -0300

    TestaPontoGr_Java - Julio - ver. Java
    Código de marcar pontos em um GUI feito em Java para rodar no BlueJ

---
+++ TestaPontoGr/App.java
@@ -0,0 +1,12 @@
+
+/**
+ * Escreva a descrição da classe Aplicacao aqui.
+ * 
+ * @author (seu nome) 
+ * @version (número de versão ou data)
+ */
+public class App {
+	public static void main(String args[]) {
+		new AppGUI(700, 900); // define dimensao da janela (em pixels)
+	}
+}
